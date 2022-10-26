3899252021b9bc17b8a22498736c8f181f92458d
Dheeraj Arora <dheeraj.arora1997@yahoo.com>
2022-10-26 13:27:18 +0530

    dot version update

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,9 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 
-mapboxgl.accessToken =
- 'pk.eyJ1IjoiZGhlZXJhamFyb3JhMTk5NyIsImEiOiJjbDlvNW4zdHMwZWRrM3VrYjNkb3B6dW5uIn0.NRSOWdRDdv_puvSI1qmUpQ';
-
+mapboxgl.accessToken = process.env.NODE_ENV === 'development' ? 'pk.eyJ1IjoiZGhlZXJhamFyb3JhMTk5NyIsImEiOiJjbDlvM2VmbTQwZHlzM3BvNW04ODU1NTJ1In0.kpZXObzDZM5VFW3y3oq99g' : 'pk.eyJ1IjoiZGhlZXJhamFyb3JhMTk5NyIsImEiOiJjbDlvNW4zdHMwZWRrM3VrYjNkb3B6dW5uIn0.NRSOWdRDdv_puvSI1qmUpQ';
 export default function Map(props) {
  const mapContainerRef = useRef(null);
-
  const [location, setLocation] = useState(props.location);
  const [lng, setLng] = useState(props.lng);
  const [lat, setLat] = useState(props.lat);
